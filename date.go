6792209ec6f413d10b8da2653f04cf7e8c8f08f6
Muflih Kholidin <muflic.24@gmail.com>
2019-09-01 06:18:53 +0700

    NewTZxx

--- date.go
+++ date.go
@@ -22,3 +22,33 @@ func NewTZ(tz string) time.Time{
 	return t
 }
 
+func NewTZLocal() time.Time{
+	now := time.Now()
+	t, err := time.Parse(YMD_HMS_TZ, now.Format(YMD_HMS) + "+0000")
+	if err != nil {
+		fmt.Println("Error create date with timezone")
+		return now
+	}
+	return t
+}
+
+func NewTZ0() time.Time{
+	now := time.Now()
+	t, err := time.Parse(YMD_HMS_TZ, now.Format(YMD_HMS) + "+0000")
+	if err != nil {
+		fmt.Println("Error create date with timezone")
+		return now
+	}
+	return t
+}
+
+func NewTZ7() time.Time{
+	now := time.Now()
+	t, err := time.Parse(YMD_HMS_TZ, now.Format(YMD_HMS) + "+0700")
+	if err != nil {
+		fmt.Println("Error create date with timezone")
+		return now
+	}
+	return t
+}
+
